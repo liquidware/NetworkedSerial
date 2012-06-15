fea09bb6e2b76e140c2f8666315d2a2174aea7d4
Chris <chris.ladden@liquidware.com>
2012-06-14 21:41:09 -0400

    Change serial port and baud rate.

--- src/com/liquidware/networkedserial/app/Application.java
+++ src/com/liquidware/networkedserial/app/Application.java
@@ -1,17 +1,17 @@
 /*
  * Copyright 2009 Cedric Priscal
- * 
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * 
+ *
  * http://www.apache.org/licenses/LICENSE-2.0
- * 
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
- * limitations under the License. 
+ * limitations under the License.
  */
 
 package com.liquidware.networkedserial.app;
@@ -24,7 +24,6 @@ import java.security.InvalidParameterException;
 import android.content.SharedPreferences;
 import android_serialport_api.SerialPort;
 import android_serialport_api.SerialPortFinder;
-import com.liquidware.networkedserial.app.R;
 
 public class Application extends android.app.Application {
 
@@ -35,8 +34,8 @@ public class Application extends android.app.Application {
 		if (mSerialPort == null) {
 			/* Read serial port parameters */
 			SharedPreferences sp = getSharedPreferences("android_serialport_api.sample_preferences", MODE_PRIVATE);
-			String path = "/dev/ttyUSB0"; //sp.getString("DEVICE", "");
-			int baudrate = 115200; //Integer.decode(sp.getString("BAUDRATE", "-1"));
+			String path = "/dev/ttyO2";
+			int baudrate = 9600;
 
 			/* Check parameters */
 			if ( (path.length() == 0) || (baudrate == -1)) {
